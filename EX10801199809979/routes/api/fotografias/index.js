0431a159276d534cf547d5e81999a388d8566897
Yonathan Cruz <yonathancruz2015@gmail.com>
2020-02-12 00:51:33 -0600

    METODO PUT TERMINADO

--- EX10801199809979/routes/api/fotografias/index.js
+++ EX10801199809979/routes/api/fotografias/index.js
@@ -5,6 +5,7 @@ var fileModel = require('../filemodel');
 
 var fotCollection = fileModel.getFotografias();
 
+ /*METODO GET*/
 router.get('/', function(req, res){
     res.json({
       "entity":"Fotografias",
@@ -15,4 +16,73 @@ router.get('/', function(req, res){
   router.get('/all', function(req, res){
     fotCollection = fileModel.getFotografias();
     res.json(fotCollection);
-  }) //GET /ALL
+  }) 
+  
+   /*METODO POST*/
+  router.post('/new', function(req, res){
+    fotCollection = fileModel.getFotografias();
+    var newFo = Object.assign(
+       {},
+       req.body,
+       {
+           "title": req.body.title,
+           "url": req.body.url,
+           "thumbnailUrl": req.body.thumbnailUrl,
+           "album": req.body.album
+       }
+    );
+    var foExists = fotCollection.find(
+      function(o, i){
+        return o.id === newFo.id;
+      }
+    )
+    if( ! foExists ){
+      fotCollection.push(newFo);
+      fileModel.setFotografias(
+         fotCollection,
+         function(err, savedSuccesfully){
+           if(err){
+             res.status(400).json({ "error": "Lo sentimos ha ocurrido un error" });
+           } else {
+             res.json(newFo);  
+           }
+         }
+       );
+    } else {
+      res.status(400).json({"error":"Lo sentimos ha ocurrido un error"});
+    }
+  }); 
+  /*METODO PUT*/
+
+  router.put('/update/:foId',
+  function(req, res){
+      fotCollection = fileModel.getFotografias();
+      var foIdToModify = req.params.foId;
+      var amountToAdjust = parseInt(req.body.ajustar);
+      var adjustType = req.body.tipo || 'SUB';
+      var adjustHow = (adjustType == 'ADD' ? 1 : -1);
+      var modFotografias = {};
+      var newFotografiasArray = fotCollection.map(
+        function(o,i){
+          if( foIdToModify === o.id){
+             o.title = adjustType;
+             modFotografias = Object.assign({}, o);
+          }
+          return o;
+        }
+      ); 
+    fotCollection = newFotografiasArray;
+    fileModel.setFotografias(
+      fotCollection,
+      function (err, savedSuccesfully) {
+        if (err) {
+          res.status(400).json({ "error": "Lo sentimos ha ocurrido un error, No se pudo actualizar" });
+        } else {
+          res.json(modFotografias);  
+        }
+      }
+    );
+  }
+);
+
+  
